e9259ddee008fea2d51809bd13a46d37d750552b
installer <installer@gmail.com>
2025-08-31 01:53:55 +0200

    Updated flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A basic Rust project";
 
   inputs = {
-    nixpkgs-src.url = "https://github.com/NixOS/nixpkgs/archive/nixos-25.05.tar.gz";
+    nixpkgs-src.url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
     flake-utils-src.url = "https://github.com/numtide/flake-utils/archive/main.tar.gz";
   };
 
@@ -14,9 +14,20 @@
       {
         devShells.default = pkgs.mkShell {
           buildInputs = [
+            pkgs.nixd
             pkgs.rustc
+            pkgs.rust-analyzer
+            pkgs.rustfmt
             pkgs.cargo
+            pkgs.openssl.dev
+            pkgs.pkg-config
+            pkgs.direnv
+            pkgs.net-snmp
           ];
+          OPENSSL_DIR = "${pkgs.openssl.dev}";
+          OPENSSL_LIB_DIR = "${pkgs.openssl.out}/lib";
+          OPENSSL_INCLUDE_DIR = "${pkgs.openssl.dev}/include";
+          LD_LIBRARY_PATH = "${pkgs.openssl.out}/lib:${pkgs.lib.makeLibraryPath [pkgs.openssl]}";
         };
       });
 }
