4298816ddeb728f70aab5a98a35e00708fecada1
installer <installer@gmail.com>
2026-09-01 02:37:07 +0200

    The first battle with Nokia SRL may be lost, but I shall win the war. Maybe. Eventually.

--- flake.nix
+++ flake.nix
@@ -39,6 +39,17 @@
           libdrm
           libllvm
         ];
+        dep_graph_rs = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "dep_graph_rs";
+          version = "0.1.0"; # Replace with target version if needed
+      
+          src = pkgs.fetchCrate {
+            inherit pname version;
+            sha256 = "sha256-1HxBc9Z58oppFmKk8etUnIClEK5q0m6OCCDtYrG0lBE="; 
+          };
+      
+          cargoHash = "sha256-+nALS2wqe6MXHGgBlKKguZRmcMTqNYyX1qppXKG4LIQ="; # Replace with actual hash after first run
+        };
       in {
         devShells.default = pkgs.mkShell {
           name = "rust-egui-dev-shell";
@@ -72,6 +83,7 @@
             containerlab
             python314            
             gnmic
+            dep_graph_rs
 
             # libwayland  # Uncomment if you need the static lib
             # nushell     # Uncomment to use nushell as login shell
